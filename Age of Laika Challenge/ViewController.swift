8bd2c80a0e13b71b386759f2af542c2cba0fad8e
Maciej Sugajski <maciejsugajski@gmail.com>
2016-07-19 13:07:56 +0200

    Initial Commit

---
+++ Age of Laika Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Age of Laika Challenge
+//
+//  Created by Maciej Sugajski on 19.07.2016.
+//  Copyright © 2016 Maciej Sugajski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
